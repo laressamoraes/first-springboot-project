07cd8e874a5ffa6cccf1e8052fe51f7f628e2dfa
Laressa Moraes <laressa.luisy@hotmail.com>
2022-05-10 16:40:03 -0300

    Payment, one-to-one association

--- src/main/java/com/grupowl/springbootproject/config/TestConfig.java
+++ src/main/java/com/grupowl/springbootproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.grupowl.springbootproject.entities.Category;
 import com.grupowl.springbootproject.entities.Order;
 import com.grupowl.springbootproject.entities.OrderItem;
+import com.grupowl.springbootproject.entities.Payment;
 import com.grupowl.springbootproject.entities.Product;
 import com.grupowl.springbootproject.entities.User;
 import com.grupowl.springbootproject.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2022-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
